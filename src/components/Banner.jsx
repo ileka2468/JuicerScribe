ee325cf78310a64ec31fc9170a149947fd3f59e1
William Ileka <ileka2468@gmail.com>
2025-01-18 20:48:27 -0600

    adjust date

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,8 +5,8 @@ export default function Banner() {
   return (
     <div className="bg-indigo-600 text-white overflow-hidden py-2">
       <div className="animate-marquee">
-        We will be adding videos and officially launching in beta on Tuesday,
-        January 18th. So clean your sticky keyboards and get ready to
+        We will be adding videos and officially launching in beta on Friday,
+        January 24th. So clean your sticky keyboards and get ready to
         transcribe! 🚀
       </div>
     </div>
