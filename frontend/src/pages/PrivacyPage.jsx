1c253b7fe8c8571ec4fdf8fe58b6be2cf389e40d
William Ileka <ileka2468@gmail.com>
2025-01-21 23:58:59 -0600

    edit policy b/c ezoic is lame

--- frontend/src/pages/PrivacyPage.jsx
+++ frontend/src/pages/PrivacyPage.jsx
@@ -96,80 +96,7 @@ export default function PrivacyPage() {
             </ul>
 
             <h2 className="text-2xl font-semibold mt-6 mb-4">
-              8. Ezoic Services
-            </h2>
-            <p className="mb-4">
-              This website uses the services of Ezoic Inc. (“Ezoic”), including
-              to manage third-party interest-based advertising. Ezoic may employ
-              a variety of technologies on this website, including tools to
-              serve content, display advertisements and enable advertising to
-              visitors of this website, which may utilize first and third-party
-              cookies.
-            </p>
-            <p className="mb-4">
-              A cookie is a small text file sent to your device by a web server
-              that enables the website to remember information about your
-              browsing activity. First-party cookies are created by the site you
-              are visiting, while third-party cookies are set by domains other
-              than the one you're visiting. Ezoic and our partners may place
-              third-party cookies, tags, beacons, pixels, and similar
-              technologies to monitor interactions with advertisements and
-              optimize ad targeting. Please note that disabling cookies may
-              limit access to certain content and features on the website, and
-              rejecting cookies does not eliminate advertisements but will
-              result in non-personalized advertising. You can find more
-              information about cookies and how to manage them{" "}
-              <a className="underline" href="https://allaboutcookies.org/">
-                here
-              </a>
-              .
-            </p>
-            <p className="mb-4">
-              The following information may be collected, used, and stored in a
-              cookie when serving personalized ads:
-            </p>
-            <ul className="list-disc list-inside mb-6">
-              <li>IP address</li>
-              <li>Operating system type and version</li>
-              <li>Device type</li>
-              <li>Language preferences</li>
-              <li>Web browser type</li>
-              <li>Email (in a hashed or encrypted form)</li>
-            </ul>
-            <p className="mb-4">
-              Ezoic and its partners may use this data in combination with
-              information that has been independently collected to deliver
-              targeted advertisements across various platforms and websites.
-              Ezoic's partners may also gather additional data, such as unique
-              IDs, advertising IDs, geolocation data, usage data, device
-              information, traffic data, referral sources, and interactions
-              between users and websites or advertisements, to create audience
-              segments for targeted advertising across different devices,
-              browsers, and apps. You can find more information about
-              interest-based advertising and how to manage them{" "}
-              <a className="underline" href="https://youradchoices.com/">
-                here
-              </a>
-              .
-            </p>
-            <p className="mb-4">
-              You can view Ezoic's privacy policy{" "}
-              <a className="underline" href="https://ezoic.com/privacy/">
-                here
-              </a>
-              , or for additional information about Ezoic's advertising and
-              other partners, you can view Ezoic's advertising partners{" "}
-              <a
-                className="underline"
-                href="https://www.ezoic.com/privacy-policy/advertising-partners/"
-              >
-                here
-              </a>
-              .
-            </p>
-
-            <h2 className="text-2xl font-semibold mt-6 mb-4">
-              9. Changes to Privacy Policy
+              8. Changes to Privacy Policy
             </h2>
             <p className="mb-4">
               We may update this privacy policy from time to time. We will
@@ -177,7 +104,7 @@ export default function PrivacyPage() {
               email.
             </p>
 
-            <h2 className="text-2xl font-semibold mt-6 mb-4">10. Contact Us</h2>
+            <h2 className="text-2xl font-semibold mt-6 mb-4">9. Contact Us</h2>
             <p className="mb-4">
               For privacy-related questions or concerns, please contact us
               through our support channels.
